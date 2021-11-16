a7d48e98fd5f302fe58a64c4b8be45addfd2da2a
prat <99pratyush@gmail.com>
2021-11-16 22:02:35 +0100

    heroku deploy additions

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/aa-test'));
+app.use(express.static(__dirname + '/dist/AA-test'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/aa-test/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/AA-test/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
